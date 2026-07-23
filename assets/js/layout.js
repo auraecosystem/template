9a8f0c069c02f3d796bd3cfe5c3eaed477f24137
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-23 01:35:25 +0100

    Create layout.js

---
+++ assets/js/layout.js
@@ -0,0 +1,12 @@
+import sdk from '@stackblitz/sdk'
+
+sdk.embedProjectId(
+  'elementOrId',
+  'vision-pro-svelte-nativescript-gtpixngv',
+  {
+    forceEmbedLayout: true,
+    openFile: 'package.json',
+    theme: 'dark',
+    clickToLoad: true,
+  }
+);
